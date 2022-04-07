e5bfa7a239740e3e18f20949dfcef436b9027ce8
Édison Krause dos Santos <edinhokrause@hotmail.com>
2022-04-07 19:19:35 -0300

    I added a conditional 'if' that closes the window when triggered the exit button and also a 'while' loop to the window kept open when triggered the playlist button

--- InterfaceFy.py
+++ InterfaceFy.py
@@ -7,11 +7,17 @@ sg.theme('Black')
 class TelaPython:
     def __init__(self):
         layout = [
-            [sg.Button('Playlist 1', size=(15, 5), key='playlist1'), sg.Button(
-                'Playlist 2', size=(15, 5), key='playlist2'), sg.Button('Playlist 3', size=(15, 5), key='playlist3')]
+            [sg.Text(size=(15, 1)), sg.Button('Playlist 1', size=(15, 5), key='playlist1'), sg.Button(
+                'Playlist 2', size=(15, 5), key='playlist2'), sg.Button('Playlist 3', size=(15, 5), key='playlist3'), sg.Text(size=(15, 1))]
         ]
-        janela = sg.Window("Playlist Spotify",).layout(layout)
-        self.button, self.values = janela.read()
+        self.janela = sg.Window("Playlist Spotify",).layout(layout)
+
+    def iniciar(self):
+        while True:
+            self.button, self.values = self.janela.read()
+            if self.values == sg.WIN_CLOSED:
+                break
 
 
 tela = TelaPython()
+tela.iniciar()
